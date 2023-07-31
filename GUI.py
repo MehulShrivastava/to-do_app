21762e0d0a12f2bc7c61f309f0463f099eff1810
MehulShrivastava <mehulshrivastava72@gmail.com>
2023-07-31 16:51:14 +0530

    complete web site

--- GUI.py
+++ GUI.py
@@ -1,6 +1,11 @@
 import functions
 import PySimpleGUI as Sg
 import time
+import os
+
+if not os.path.exists("todo.txt"):
+    with open("todos.txt") as file:
+        pass
 
 Sg.theme("BluePurple")
 
